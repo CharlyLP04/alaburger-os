474b8d091c427477b288997e9b0f89d6f70928a0
CharlyLP04 <col2804040204@gmail.com>
2026-07-19 00:18:59 -0600

    fix: agregar configuracion ssl requerida por Render/Vercel en produccion

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
+  ssl: process.env.NODE_ENV === 'production' || process.env.VERCEL ? { rejectUnauthorized: false } : false,
 });
 
 pool.on('error', (err) => {
